34d74656ee772664ce8b8e63466af8ab1d9bae74
yangliangfei <yangliangfei@oneband.life>
2018-03-27 23:19:25 +0800

    [UPDATE] 电影列表增加 关键词 搜索

--- src/routes/rank.js
+++ src/routes/rank.js
@@ -13,19 +13,30 @@ export default class RankRouter {
   @request('post', '/rank')
   @query({
     page: { type: 'number', require: false, default: 1, description: '页码' },
-    limit: { type: 'number', require: false, default: 10, description: '每页数量' }
+    limit: { type: 'number', require: false, default: 10, description: '每页数量' },
+    searchWord: { type: 'string', require: false, description: '搜索词' }
   })
   @tag
   @summary('电影排行表')
   static async getRank(ctx) {
-    const { page, limit } = ctx.validatedQuery;
-    const { count, rows: ranks } = await Rank.findAndCount({
-      page,
-      limit,
-      offset: (page - 1) * limit,
-      order: [['order', 'ASC']]
-    });
+    const { page, limit, searchWord } = ctx.validatedQuery;
 
-    ctx.body = { count, ranks };
+    if (searchWord) {
+      const { rows: ranks } = await Rank.findAndCountAll({
+        where: { title: { $like: `${searchWord}%` } },
+        limit,
+        order: [['order', 'ASC']]
+      });
+      ctx.body = { ranks };
+    } else {
+      const { count, rows: ranks } = await Rank.findAndCount({
+        page,
+        limit,
+        offset: (page - 1) * limit,
+        order: [['order', 'ASC']]
+      });
+      ctx.body = { count, ranks };
+    }
   }
+
 }
