cf8505f4a2e9363f479d489f88ce31b86591396d
yangliangfei <yangliangfei@oneband.life>
2018-04-08 21:06:12 +0800

    [UPDATE] 电影列表增加类型筛选

--- src/routes/rank.js
+++ src/routes/rank.js
@@ -14,12 +14,26 @@ export default class RankRouter {
   @query({
     page: { type: 'number', require: false, default: 1, description: '页码' },
     limit: { type: 'number', require: false, default: 10, description: '每页数量' },
-    searchWord: { type: 'string', require: false, description: '搜索词' }
+    searchWord: { type: 'string', require: false, description: '搜索词' },
+    type: { type: 'string', require: false, description: '电影类型' },
+    area: { type: 'string', require: false, description: '电影区域' },
+    time: { type: 'string', require: false, description: '电影时间' }
   })
   @tag
   @summary('电影排行表')
   static async getRank(ctx) {
     const { page, limit, searchWord } = ctx.validatedQuery;
+    let { type, area, time } = ctx.validatedQuery;
+    // console.log(`${type}${area}${time}`);
+    if (type === '全部') type = '';
+    if (area === '全部') area = '';
+    if (time === '全部') {
+      time = '';
+    } else if (time === '1970') {
+      time = [1900, 1969];
+    } else {
+      time = time.split(',');
+    }
 
     if (searchWord) {
       const { rows: ranks } = await Rank.findAndCountAll({
@@ -30,6 +44,8 @@ export default class RankRouter {
       ctx.body = { ranks };
     } else {
       const { count, rows: ranks } = await Rank.findAndCount({
+        where: time ? { type: { $like: `%${type}%` }, area: { $like: `%${area}%` }, time: { $between: time } }
+                    : { type: { $like: `%${type}%` }, area: { $like: `%${area}%` } },
         page,
         limit,
         offset: (page - 1) * limit,
