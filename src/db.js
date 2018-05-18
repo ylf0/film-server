c2adbbaea72a8d319bcbe14358ed73c9e7235669
yangliangfei <yangliangfei@oneband.life>
2018-05-18 09:13:41 +0800

    test

--- src/db.js
+++ src/db.js
@@ -1,17 +1,15 @@
 import Sequelize from 'sequelize';
 
-// import config from 'config';
+import config from 'config';
 
-// const { host, database, user, password } = config.mysql;
+const { host, database, user, password } = config.mysql;
 
-// let mysqlUrl;
+let mysqlUrl;
 
-// if (password) {
-//   mysqlUrl = `mysql://${user}:${password}@${host}/${database}`;
-// } else {
-//   mysqlUrl = `mysql://${user}@${host}/${database}`;
-// }
-
-const mysqlUrl = 'mysql://root:zhinan@127.0.0.1/film';
+if (password) {
+  mysqlUrl = `mysql://${user}:${password}@${host}/${database}`;
+} else {
+  mysqlUrl = `mysql://${user}@${host}/${database}`;
+}
 
 export default new Sequelize(mysqlUrl, { logging: false, timezone: '+08:00' });
